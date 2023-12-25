0163723175cca5ae2efde8893d4008b82d7fd08c
Ritter Gustave <rGustave5@gmail.com>
2023-12-24 22:21:41 -0800

    refactor(subscriptions): add stricter validation rules

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -9,20 +10,42 @@ pub struct FormData {
     name: String,
 }
 
+pub fn parse_subscriber(form_data: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form_data.name)?;
+    let email = SubscriberEmail::parse(form_data.email)?;
+
+    Ok(NewSubscriber { email, name })
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+
+        Ok(Self { email, name })
+    }
+}
+
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool),
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+// pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(pool: &PgPool, new_subscriber: &NewSubscriber) -> Result<(), sqlx::Error> {
+    let utc_now = Utc::now();
+    let unique_id = Uuid::new_v4();
+
     sqlx::query!(
         r#"
                 INSERT INTO subscriptions (id, email, name, subscribed_at)
                 VALUES ($1, $2, $3, $4)
             "#,
-        Uuid::new_v4(),
-        form.email,
-        form.name,
-        Utc::now()
+        unique_id,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
+        utc_now
     )
     .execute(pool)
     .await
@@ -47,7 +70,12 @@ pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sql
     )
 ]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    match insert_subscriber(&pool, &form).await {
+    let new_subscriber = match form.0.try_into() {
+        Ok(subscriber) => subscriber,
+        Err(_) => return HttpResponse::BadRequest().finish()
+    };
+
+    match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
